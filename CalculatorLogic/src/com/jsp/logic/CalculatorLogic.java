c62c0c75a932d50c26c007252cd777c5ae4809ad
Ranjitapatil-ai <ranjitapatil8431@gmail.com>
2025-02-01 16:43:15 +0530

    Add files via upload

---
+++ CalculatorLogic/src/com/jsp/logic/CalculatorLogic.java
@@ -0,0 +1,19 @@
+package com.jsp.logic;
+
+public class CalculatorLogic {
+	public int add(int x,int y) {
+		return x+y;
+	}
+
+	public int sub(int x,int y) {
+		return x-y;
+	}
+
+	
+	public int mul(int x,int y) {
+		return x*y;
+	}
+
+	
+	
+}
